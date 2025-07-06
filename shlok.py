49a565b6de39e04cd75aafa3a3341e533d4885f3
Shlok-Vertex <mrmanishkr7911@gmail.com>
2025-07-06 21:48:11 +0530

    add more codes

--- shlok.py
+++ shlok.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
 l = [1,2,3,4,5]
+x = [1, 2, 3, 4, 5]
 
 var = pd.DataFrame(l)
+var1 = pd.Series(x,index=['a', 'b', 'c', 'd', 'e'], name='numbers')
 print(var)
+print(var1)
+print(type(var))
+print(type(var1))
